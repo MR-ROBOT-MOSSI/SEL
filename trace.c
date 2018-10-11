f5cd159950bc6f8d1e917d075bfe3d18fd27b365
abdoully <lyabdoul30@gmail.com>
2018-10-11 17:46:14 +0200

    new branche

--- trace.c
+++ trace.c
@@ -6,11 +6,8 @@
 #include <errno.h>
 #include <sys/user.h>
 
-#define bufSize 1024
-
 int main (int argc, char *argv[])
 {	
-	//system("ls -l");
 	/**
 	*	Déclaration des variables 
 	**/
@@ -18,88 +15,23 @@ int main (int argc, char *argv[])
 	char *adresse_fonction2[50]; 
 	char trap = 0xCC;
 	char *fic[30]; 
-	FILE* fichier ;
-	char *temp0[30];
-	char *temp1[30];
-	char *temp2[30];  
+	FILE* fichier ; 
 	
-	FILE *fp;
-	FILE *f_addr;
-	
-	int ret, ret2;
-	char ch;
-	
-	char buf[bufSize];
-	char buf2[bufSize];
-
 	/**
 	* 	Récupération des arguments 
 	**/
 
 	pid_t pid = atoi(argv[1]); 
 	adresse_fonction = argv[2];
+	 
+	//Concaténation @ mémoire + conversion en long  
+	sprintf(adresse_fonction2,"0x%s",adresse_fonction);	
+	long var = strtol(adresse_fonction2,&adresse_fonction2,16);	
 	
+
 	/**
 	*
 	**/
-	sprintf(temp0, "readlink /proc/%d/exe > chemin_trace.txt", pid);
-	system(temp0);
-	
-	fp = fopen("chemin_trace.txt", "r"); // read mode
- 
-   	if (fp == NULL)
-   	{
-    	perror("Error while opening the file chemin path code.\n");
-    	exit(EXIT_FAILURE);
-   	}
-   	
-   	//printf("The contents of %s file are:\n", file_name);
-   	
-   	while (fgets(buf, sizeof(buf), fp) != NULL){
-   	 	buf[strlen(buf) - 1] = '\0'; // eat the newline fgets() stores
-    	//printf("%s\n", buf);
-  	}
-   	
-   	
-   	
-   	fclose(fp);
-   	ret = remove("chemin_trace.txt");
-   	
-   	/*if(ret == 0) {
-      	printf("File deleted successfully");
-   	} else {
-      	printf("Error: unable to delete the file");
-   	}*/
-      
-   	sprintf(temp1, "nm %s | grep ecrire | cut -d ' ' -f1 > adresse.txt", buf);
-	system(temp1);
-	
-	
-	f_addr = fopen("adresse.txt", "r"); // read mode
- 
-   	if (fp == NULL)
-   	{
-    	perror("Error while opening the file adresse.\n");
-    	exit(EXIT_FAILURE);
-   	}
-   	
-   	while (fgets(buf2, sizeof(buf2), f_addr) != NULL){
-   	 	buf2[strlen(buf2) - 1] = '\0'; // eat the newline fgets() stores
-    	//printf("%s\n", buf2);
-  	}
-  	
-  	fclose(f_addr);
-   	ret2 = remove("adresse.txt");
-	
-	
-	//nm /home/abdoul/Bureau/TP_SEL_2018/code | grep ecrire | cut -d ' ' -f1
-	 
-	//Concaténation @ mémoire + conversion en long  
-	printf("%s\n", buf2);
-	sprintf(adresse_fonction2,"0x%s",buf2);	
-	long var = strtol(adresse_fonction2,&adresse_fonction2,16);	
-
-	
 
 	int att = ptrace(PTRACE_ATTACH, pid, NULL, NULL); //Attachement au processus
 
@@ -136,12 +68,6 @@ int main (int argc, char *argv[])
 	}
 	
 		ptrace(PTRACE_CONT,pid,NULL,NULL);
-		printf("Test Avant WAIT");
-		wait(NULL);
-		printf("TEST Après WAIT");
-		ptrace(PTRACE_DETACH,pid,NULL,NULL);
-		
-		return 0;
-}
 
-//texte hasher + chiffrer est-il suifissant 
+		ptrace(PTRACE_DETACH,pid,NULL,NULL);
+}
